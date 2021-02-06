77a0e302bcbf66a88a550f15198bcd4ee6373a5f
Chang Zhe Jiet <zhejiet.chang@ufinity.com>
2021-02-06 22:04:33 +0800

    jest.config.json

--- test/index.test.ts
+++ test/index.test.ts
@@ -6,18 +6,18 @@ const asyncFnNoArg = (): Promise<number> => new Promise(resolve => resolve(3));
 const asyncError = (): Promise<number> => new Promise((resolve, reject) => reject('test'));
 
 describe('Multiple args', () => {
-  test('toSync', async () => {
+  test('toSync', () => {
     const syncFn = AsyncUtil.toSync(asyncFnMultiArgs);
     const result = syncFn([1, 2]);
     expect(result).toBe(3);
   });
 
-  test('executeSync', async () => {
+  test('executeSync', () => {
     const result = AsyncUtil.executeSync(() => asyncFnMultiArgs(1, 2));
     expect(result).toBe(3);
   });
 
-  test('resolvePromise', async () => {
+  test('resolvePromise', () => {
     const promise = asyncFnMultiArgs(1, 2);
     const result = AsyncUtil.resolvePromise(promise);
     expect(result).toBe(3);
@@ -25,7 +25,7 @@ describe('Multiple args', () => {
 });
 
 describe('One arg', () => {
-  test('toSync', async () => {
+  test('toSync', () => {
     const syncFn = AsyncUtil.toSync(asyncFnOneArg);
     let result = syncFn(1);
     expect(result).toBe(3);
@@ -33,12 +33,12 @@ describe('One arg', () => {
     expect(result).toBe(3);
   });
 
-  test('executeSync', async () => {
+  test('executeSync', () => {
     const result = AsyncUtil.executeSync(() => asyncFnOneArg(1));
     expect(result).toBe(3);
   });
 
-  test('resolvePromise', async () => {
+  test('resolvePromise', () => {
     const promise = asyncFnOneArg(1);
     const result = AsyncUtil.resolvePromise(promise);
     expect(result).toBe(3);
@@ -46,7 +46,7 @@ describe('One arg', () => {
 });
 
 describe('No arg', () => {
-  test('toSync', async () => {
+  test('toSync', () => {
     const syncFn = AsyncUtil.toSync(asyncFnNoArg);
     let result = syncFn();
     expect(result).toBe(3);
@@ -54,19 +54,19 @@ describe('No arg', () => {
     expect(result).toBe(3);
   });
 
-  test('executeSync', async () => {
+  test('executeSync', () => {
     const result = AsyncUtil.executeSync(() => asyncFnNoArg());
     expect(result).toBe(3);
   });
 
-  test('resolvePromise', async () => {
+  test('resolvePromise', () => {
     const promise = asyncFnNoArg();
     const result = AsyncUtil.resolvePromise(promise);
     expect(result).toBe(3);
   });
 });
 
-test('Error', async () => {
+test('Error', () => {
   const syncFn = AsyncUtil.toSync(asyncError);
   expect(() => syncFn()).toThrow('test');
 });
